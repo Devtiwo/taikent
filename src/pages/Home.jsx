b7a051c4d353867280816f4d2937f34a82854ece
Taiwo Adetunji <dontiwo5020@yahoo.com>
2024-09-08 15:02:52 +0100

    Added components for the home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import Header from '../components/Header';
+import About from '../components/About';
+import Benefits from '../components/Benefits';
+import Join from '../components/Join';
+import Pricing from '../components/pricing';
+import Testimonials from '../components/Testimonials';
+import Footer from '../components/Footer';
 
 const Home = () => {
   return (
     <>
       <Header />
+      <About />
+      <Benefits />
+      <Join />
+      <Pricing />
+      <Testimonials />
+      <Footer />
     </>
   )
 }
